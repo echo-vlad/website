bfed14232b932f303c342ccd1037f015166d2538
Vlad Creanga <61327878+echo-vlad@users.noreply.github.com>
2026-02-06 22:16:21 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+const container = document.getElementById("container");
+
+noBtn.addEventListener("click", () => {
+  message.textContent = "Wrong answer 😅 Try again.";
+});
+
+yesBtn.addEventListener("click", () => {
+  container.innerHTML = "<h1>I love you ❤️</h1>";
+});
